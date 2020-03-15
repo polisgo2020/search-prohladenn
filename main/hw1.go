4548e46764855e6f0bc60a6a5edc983e191614b2
valery.kovshov <valery.kovshov@team.wrike.com>
2020-03-15 19:56:08 +0300

    Fix homework 1 after review

--- main/hw1.go
+++ main/hw1.go
@@ -3,42 +3,53 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
+	"unicode"
 )
 
 func check(e error) {
 	if e != nil {
-		panic(e)
+		log.Fatal(e)
 	}
 }
 
+func splitter(c rune) bool {
+	return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+}
+
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Incorrect number of input arguments")
+	}
 	pathToFolderWithFiles := os.Args[1]
-	data := make(map[string][]int)
+	data := make(map[string][]string)
 	files, err := ioutil.ReadDir(pathToFolderWithFiles)
 	check(err)
-	for fileIndex, file := range files {
-		fileBytes, err := ioutil.ReadFile(pathToFolderWithFiles + "/" + file.Name())
+	for _, file := range files {
+		pathToInputFile := filepath.Join(pathToFolderWithFiles, file.Name())
+		fileBytes, err := ioutil.ReadFile(pathToInputFile)
 		check(err)
 		fileText := string(fileBytes)
-		words := strings.Split(fileText, " ")
-		for i := 0; i < len(words); i++ {
-			word := words[i]
+		words := strings.FieldsFunc(fileText, splitter)
+		for i := range words {
+			word := strings.ToLower(words[i])
 			if data[word] == nil {
-				sliceWithOneField := []int{fileIndex}
-				data[word] = sliceWithOneField
-			} else if data[word][len(data[word])-1] != fileIndex {
-				data[word] = append(data[word], fileIndex)
+				data[word] = []string{file.Name()}
+			} else if data[word][len(data[word])-1] != file.Name() {
+				data[word] = append(data[word], file.Name())
 			}
 		}
 	}
-	outFile, err := os.Create(pathToFolderWithFiles + "/output.txt")
+	pathToOutputFile := filepath.Join(pathToFolderWithFiles, "/output.txt")
+	outFile, err := os.Create(pathToOutputFile)
 	check(err)
 	defer outFile.Close()
 	for key, value := range data {
 		stringSlice := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(value)), ","), "[]")
-		outLine := fmt.Sprintf("\"%s\":{%s}\n", key, stringSlice)
+		outLine := fmt.Sprintf("%s:{%s}\n", key, stringSlice)
 		_, err := outFile.WriteString(outLine)
 		check(err)
 	}
